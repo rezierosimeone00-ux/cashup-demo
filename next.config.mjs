c5df6b82c9060a3a1bf100e2fd4991474dd87aef
Reziero Simeone <rezierosimeone@macbookpro.home>
2025-10-30 20:58:59 -0400

    Primo commit: setup app

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ["http://localhost:3000","http://127.0.0.1:3000"]
+  }
+};
+export default nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#D4AF37",
+        black: "#0B0B0B",
+      },
+    },
+  },
+  plugins: [],
+};
